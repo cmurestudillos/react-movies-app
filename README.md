0b9dab71e4a4cea984ead95e4f527e9a0ebe97e6
cmur85 <39341418+cmurestudillos@users.noreply.github.com>
2022-09-19 12:57:46 +0200

    Deploy to Vercel Platform

---
+++ README.md
@@ -0,0 +1,16 @@
+<p align="center">
+  <a href="https://www.buymeacoffee.com/cmur" rel=”noopener noreferrer”><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=cmur&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"></a>
+</p>
+
+# Movies Simple App
+
+Buscador basico de peliculas utilizando la API [themoviedb](https://www.themoviedb.org).
+
+## Herramientas
+* Create React App => [Create React App](https://github.com/facebook/create-react-app).  
+
+## Para ejecutar esta aplicación:
+* Clonar el repositorio: 'https://github.com/cmurestudillos/movies-simple-app.git'
+* Hay que tener instalado [npm](https://www.npmjs.com) y [node](https://nodejs.org/es/)
+* Siatuarse en el directorio raiz del proyecto e instalar las dependencias: `npm install`
+* Ejecutar el siguiente comando para arrancar la aplicación: `npm start`
---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import Movie from "./components/Movie";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e21f7f3e901dfcfcecfbdce8b24628ff";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=e21f7f3e901dfcfcecfbdce8b24628ff&query=";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.results));
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+
+    if (searchTerm) {
+      getMovies(`${SEARCH_API}${searchTerm}`);
+      setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  return (
+    <>
+      <header>
+        <h1 className="title">Movies App</h1>
+        <form className="searchform" onSubmit={handleOnSubmit}>
+          <input
+            type="search"
+            className="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
+  );
+}
+
+export default App;
